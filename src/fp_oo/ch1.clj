2e474bb58005e8fd43642866ef2a4218a6635a12
Raoul Millais <raoul@raoulmillais.com>
2013-12-27 18:57:06 +0000

    Exercises 1-5

---
+++ src/fp_oo/ch1.clj
@@ -0,0 +1,71 @@
+(ns fp_oo.ch1)
+
+;; 1.10 Lists
+;; Exercise 1
+(def second 
+  (fn [l] (nth l 1)))
+
+;; Exercise 2
+(def third-nth
+  (fn [l] (nth l 2)))
+
+(def third-rest
+  (fn [l]
+    (first (rest (rest l)))))
+
+;; 1.18
+;; Exercise 3
+(def add-squares
+  (fn [& args]
+    (apply + (map * args args))))
+
+;; Exercise 4
+(def range-fac
+  (fn [n]
+    (apply * (range 1 (inc n)))))
+
+;; Exercise 5
+(def first-5
+  (fn [l]
+    (take 5 l)))
+
+(def count-duplicates
+  (fn [l]
+    (- (count l)
+       (count (distinct l)))))
+
+(def odds-and-evens 
+  (fn [l1 l2]
+    (concat (filter odd? l1)
+            (filter even? l2))))
+
+(def repeat-range
+  (fn [n]
+    (map 
+      (fn [m] 
+        (repeat m m)) 
+      (range 1 (inc n)))))
+
+(def star-struck
+  (fn [l]
+    (interleave l
+                (repeat (count l) '*))))
+
+(def chop-shop
+  (fn [l1 l2]
+    (concat (drop-last (quot (count l1) 2) l1)
+            (drop (quot (count l2) 2) l2))))
+
+(def ranges
+  (fn [l]
+    (flatten (map (fn [n] (repeat n n)) l))))
+
+(def pairs
+  (fn [l] (partition 2 l)))
+
+(def only-odd-numbers?
+  (fn [l] (every? odd? l)))
+
+(def strip-prefixed
+  (fn [prefix l]
+    (remove (fn [i] (.startsWith i prefix)) l)))
---
+++ test/fp_oo/t_ch1.clj
@@ -0,0 +1,53 @@
+(ns fp_oo.t-ch1
+  (:use midje.sweet)
+  (:use [fp_oo.ch1]))
+
+(facts "about `second`"
+  (fact "it returns the second element of a list"
+    (second '(1 2 3)) => 2))
+
+(fact "third"
+      (third-nth '(1 2 3)) => 3
+      (third-rest '(1 2 3)) => 3)
+
+(fact "add-squares"
+      (add-squares 1 2 5) => 30)
+
+(fact "range-fac"
+      (range-fac 5) => 120)
+
+(fact "first-5"
+      (first-5 [1 2 3 4 5 6 7]) => [1 2 3 4 5])
+
+(fact "count-duplicates"
+      (count-duplicates [ 1 2 2 3 3 3 4 4 4 4]) => 6)
+
+(fact 
+  "odds-and-evens creates a seq with the odds from the first and evens "
+  "from the second"
+
+      (odds-and-evens [1 2 3 4] [5 6 7 8]) => [1 3 6 8])
+
+(fact "repeat-range creates a seq of seqs with count matching item"
+      (repeat-range 4) => '((1) (2 2) (3 3 3) (4 4 4 4)))
+
+(fact "star-struck intersperses a seq with stars"
+      (star-struck [1 2 3 4 5]) => '[1 * 2 * 3 * 4 * 5 *])
+
+(fact "chop-shop creates a seq comprised the first half of the first"
+      "and the second half of the second"
+      (chop-shop [1 2 3 4] [5 6 7 8]) => [1 2 7 8])
+
+(fact "ranges"
+      (ranges [4 2 3]) => [4 4 4 4 2 2 3 3 3])
+
+(fact "pairs"
+      (pairs [1 2 3 4 5 6 7 8]) => [[1 2] [3 4] [5 6] [7 8]])
+
+(fact "only-odd-numbers?"
+      (only-odd-numbers? [ 1 3 5 7]) => true
+      (only-odd-numbers? [ 1 2 3 4]) => false)
+
+(fact "strip-prefixed"
+      (strip-prefixed "foo" ["foobar" "barbar" "foobaz" "bazquux" "foo"])
+      => ["barbar" "bazquux"])
